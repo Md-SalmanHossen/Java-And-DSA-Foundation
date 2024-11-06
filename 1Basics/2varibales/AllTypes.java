c9010095d092c020325bca9b640f24b2dc6b5e19
SalmanHossen <hossainsalman873@gmail.com>
2024-11-06 16:16:04 +0600

    practice all basic variable types

---
+++ 1Basics/2varibales/AllTypes.java
@@ -0,0 +1,20 @@
+
+public class AllTypes {
+   public static void main(String[] args) {
+
+      int number=20;//Stores integers
+      double price=20.5;//Stores floating-point numbers 
+      char grade='A';//Stores single characters
+      boolean isJavaFun=true;// Stores true or false values
+      boolean isJavaBoring=false;
+      String name="Salman";//Stores text
+
+      System.out.println(number);
+      System.out.println(price);
+      System.out.println(grade);
+      System.out.println(isJavaFun);
+      System.out.println(isJavaBoring);
+      System.out.println(name);
+
+   }
+}
