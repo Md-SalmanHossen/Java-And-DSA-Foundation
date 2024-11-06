f7c52dbf0a63f5aa0a48651198159f3f306d4c3f
SalmanHossen <hossainsalman873@gmail.com>
2024-11-06 16:25:42 +0600

    practice arithmetic operators

---
+++ 1Basics/3operators/ArithmeticOperator.java
@@ -0,0 +1,26 @@
+
+public class ArithmeticOperator {
+   public static void main(String[] args) {
+      
+      int num1=10;
+      int num2=5;
+
+      int sum=num1+num2;
+      int addition=num1+num2+3;
+      System.out.println(sum);
+      System.out.println(addition);
+
+      int sub=num1-num2;
+      System.out.println(sub);
+
+      int mul=num1*num2;
+      System.out.println(mul);
+
+      int div=num1/num2;
+      System.out.println(div);
+
+      int mod=num1%num2;
+      System.out.println(mod);
+
+   }
+}
