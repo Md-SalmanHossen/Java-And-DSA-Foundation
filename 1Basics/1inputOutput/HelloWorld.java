8c6292032b4db86db6e1295acf7e8acf2a125dac
SalmanHossen <hossainsalman873@gmail.com>
2024-11-06 16:06:20 +0600

    Java And DSA Foundation

---
+++ 1Basics/1inputOutput/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld{
+   public static void main(String[] args) {
+      System.out.println("Hello world");// print with new line
+      System.out.println("=================");
+      System.out.print("hello");//print in single line
+      System.out.print(" world");
+   }
+}
